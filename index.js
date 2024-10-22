316d192c3921649825f83fe690dbb80f585a0ab6
jdav892 <jadavila9@gmail.com>
2024-10-21 22:45:49 -0400

    rotates the object and adds vertices to the icosahedron

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true
 controls.dampingFactor = 0.03;
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2); //primitves in Threejs library
+const geo = new THREE.IcosahedronGeometry(1.0, 5); //primitves in Threejs library
 const mat = new THREE.MeshStandardMaterial({
     color: 0xffffff,
     flatShading: true
@@ -40,7 +40,7 @@ wireMesh.scale.setScalar(1.001);
 //adds wiremesh to child of mesh
 mesh.add(wireMesh);
 
-const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+const hemiLight = new THREE.HemisphereLight(0x0D47A1, 0xB71C1C);
 //to change lighting of object
 scene.add(hemiLight);
 
@@ -48,7 +48,7 @@ function animate(t = 0){
 //animates object
     requestAnimationFrame(animate);
 //rotation over y axis with speed modifier
-    //mesh.rotation.y = t * 0.0001;
+    mesh.rotation.y = t * 0.0005;
     renderer.render(scene, camera);
     controls.update();
 
