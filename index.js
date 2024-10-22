b4fb25bae0d252f188af9ea7340380f2cf5fb96f
jdav892 <jadavila9@gmail.com>
2024-10-21 21:57:20 -0400

    dom renders dom element

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import * as THREE from "three"
 
+const w = window.innerWidth;
+const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
