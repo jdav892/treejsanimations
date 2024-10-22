de0be5f32d540042932506cb38334527d0cef983
jdav892 <jadavila9@gmail.com>
2024-10-21 22:23:27 -0400

    animates both the object and wireframe around it

--- index.js
+++ index.js
@@ -18,12 +18,34 @@ camera.position.z = 2; //to move camera back a bit more
 const scene = new THREE.Scene();
 
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2); //primitve in Threejs library
-const mat = new THREE.MeshBasicMaterial({
-    color: 0xccff
+const geo = new THREE.IcosahedronGeometry(1.0, 2); //primitves in Threejs library
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
 });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
+const wireMat = new THREE.MeshBasicMaterial({
+//creates wireframe around object
+    color: 0xffffff,
+    wireframe: true
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+//adds wiremesh to child of mesh
+mesh.add(wireMesh);
 
-renderer.render(scene, camera);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+//to change lighting of object
+scene.add(hemiLight);
+
+function animate(t = 0){
+//animates object
+    requestAnimationFrame(animate);
+//rotation over y axis with speed modifier
+    mesh.rotation.y = t * 0.0001;
+    renderer.render(scene, camera);
+}
+
+animate();
