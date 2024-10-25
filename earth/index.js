88c5eb0a7937bf6ef85efc9ff4357ad2fec4c2fb
jdav892 <jadavila9@gmail.com>
2024-10-25 17:53:10 -0400

    feat: uses a layer of earth map with lights over the used map to make a sphere that represents the activity on earth

--- earth/index.js
+++ earth/index.js
@@ -5,7 +5,7 @@ import makeStarfield from "./assets/src/makeStarfield.js"
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 2000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
@@ -21,29 +21,44 @@ const loader = new THREE.TextureLoader();
 const earthGroup = new THREE.Group();
 earthGroup.rotation.z = -23.4 * Math.PI / 100;
 scene.add(earthGroup)
-
-const geometry = new THREE.IcosahedronGeometry(1, 12);
+const detail = 12; 
+const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshStandardMaterial({
-    map: loader.load("./assets/textures/8k_earth_nightmap.jpg")
+    map: loader.load("./assets/textures/8k_earth_daymap.jpg")
+    //map: loader.load("./assets/textures/8k_earth_nightmap.jpg")
     //map: loader.load("./assets/textures/moonmap4k.jpg")
     //map: loader.load("./assets/textures/earthmap1k.jpg")
     //map: loader.load("./assets/textures/8k_mars.jpg")
 });
 const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
 
-const stars = makeStarfield();
+const lightsMat = new THREE.MeshBasicMaterial({
+    //color: 0x00ff00,
+    //transparent: true,
+    map: loader.load("./assets/textures/8k_earth_nightmap.jpg"),
+    blending: THREE.AdditiveBlending,
+})
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh)
+const stars = makeStarfield({numStars: 2000});
 scene.add(stars);
 
 //adds lighting around hemisphere to discern vertices
-const hemiLight =  new THREE.HemisphereLight();
-scene.add(hemiLight);
+//const hemiLight =  new THREE.HemisphereLight();
+//scene.add(hemiLight);
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-2, 0.5, 1.5)
+scene.add(sunLight)
+
+
 
 function animate(){
     requestAnimationFrame(animate);
-    renderer.render(scene, camera)
     //earthMesh.rotation.x += 0.001
     earthMesh.rotation.y += 0.001
+    lightsMesh.rotation.y += 0.001
+    renderer.render(scene, camera)
 }
 animate();
 
