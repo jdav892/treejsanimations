d8eab56c426968140360158c9a0892c58a0b605d
jdav892 <jadavila9@gmail.com>
2024-10-30 01:34:56 -0400

    creates a line with the points generated from spline.js

---
+++ wormhole/index.js
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js"
+import spline from "./spline.js";
+
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true
+controls.dampingFactor = .03;
+
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({color: 0xff0000});
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+
+const hemiLight =  new THREE.HemisphereLight(0xfff);
+scene.add(hemiLight);
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera)
+}
+animate();
+
+function handleWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    render.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', handleWindowResize, false);
